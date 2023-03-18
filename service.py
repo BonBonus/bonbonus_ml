5b1dc74038ee0651db0161fd0940084f5353d5ab
Evgenii Vigurskii <evigurskii@gmail.com>
2023-03-18 23:42:37 +0400

    update models, add rating query arg

--- service.py
+++ service.py
@@ -19,9 +19,11 @@ api = Flask(__name__)
 def get_predict_bonus():
   args = request.args
   bonus = []
-  if 'client_id' in args and 'store_id' in args:
-    bonus, prob = predict_bonus(int(args['client_id']), int(args['store_id']), gbc_clf, client_db, merchant_db, service_params)
-  return json.dumps([{"bonus": bonus, 'probability': prob}])
+  if 'client_id' in args and 'store_id' in args and 'rating' in args:
+    bonus, prob = predict_bonus(int(args['client_id']), int(args['store_id']), float(args['rating']) gbc_clf, client_db, merchant_db, service_params)
+    return json.dumps([{"bonus": bonus, 'probability': prob}])
+  else:
+    return "Missing values: client_id, store_id, rating"
 
 @api.route('/predict_neighbours', methods=['GET'])
 def get_predict_neighbours():
@@ -29,23 +31,25 @@ def get_predict_neighbours():
     neighbours = ''
     if 'client_id' in args:
       neighbours, similarity = predict_neighbours(int(args['client_id']), nbrs_clf, client_db, service_params)
-    return json.dumps([{"neighbours": neighbours.tolist()}])
+    return json.dumps([{"neighbours": neighbours.tolist(), 'similarity': similarity}])
   
 @api.route('/health', methods=['GET'])
 def health():
     return "OK"
 
 if __name__ == '__main__':
-    with zipfile.ZipFile('data/gbc.zip', 'r') as zip_ref:
-      zip_ref.extractall('data/')
-    with zipfile.ZipFile('data/nbrs.zip', 'r') as zip_ref:
-      zip_ref.extractall('data/')
-    gbc_clf = pickle.load(open('data/gbc.clf', 'rb'))
-    nbrs_clf = pickle.load(open('data/nbrs.clf', 'rb'))
-    service_params = pickle.load(open('data/service.params', 'rb'))
-    client_db = pd.read_csv('data/test_db.csv')
-    merchant_db = pd.read_csv('data/merchants_db.csv')
-    try:
-      api.run(host='0.0.0.0', port=os.getenv('PORT'))
-    except:
-      print("failed to run on port 80, rerun on default")
+  with zipfile.ZipFile('data/gbc.zip', 'r') as zip_ref:
+    zip_ref.extractall('data/')
+  with zipfile.ZipFile('data/nbrs.zip', 'r') as zip_ref:
+    zip_ref.extractall('data/')
+  gbc_clf = pickle.load(open('data/gbc.clf', 'rb'))
+  nbrs_clf = pickle.load(open('data/nbrs.clf', 'rb'))
+  service_params = pickle.load(open('data/service.params', 'rb'))
+  client_db = pd.read_csv('data/test_db.csv')
+  merchant_db = pd.read_csv('data/merchants_db.csv')
+  port = os.getenv('PORT')
+  port = 5000 if not port else port
+  try:
+    api.run(host='0.0.0.0', port=port)
+  except:
+    print("failed to run on port 80, rerun on default")
--- utils.py
+++ utils.py
@@ -25,24 +25,23 @@ def vectorize_for_bonus_predict(row, merchant_id, merchants_db, params: ServiceP
         bonuses_available = ast.literal_eval(current_merchant.bonuses_available)
     else:
         bonuses_available = current_merchant.bonuses_available
-    for i in range(0, params.max_number_purchase):
-        if i < len(purchase_history):
-            vector.append(params.merchantName2id[purchase_history[i][0]]+1)
-            vector.append(purchase_history[i][1])
-        else:
-            vector.extend([0, 0.0])
+    purchase_vector = [0] * len(params.merchantName2id)
+    for i in range(0, len(purchase_history)):
+        merchant_id = params.merchantName2id[purchase_history[i][0]]
+        purchase_vector[merchant_id] += purchase_history[i][1]
+    vector.extend(purchase_vector)
     vector.append(params.merchantName2id[current_merchant_mcc]+1)
-    for i in range(0, len(params.bonus_type)):
-        if i < len(bonuses_available):
-            vector.append(params.bonusName2id[bonuses_available[i]]+1)
-        else:
-            vector.append(0)
+    bonus_vector = [0] * len(params.bonusName2id)
+    for i in range(0, len(bonuses_available)):
+        bonus_id = params.bonusName2id[bonuses_available[i]]
+        bonus_vector[bonus_id] = 1
+    vector.extend(bonus_vector)
     vector.append(rating)
     if train:
         if bonus_to_offer:
-            vector.append(params.bonusName2id[bonus_to_offer]+1)
+            vector.append(params.bonusName2id[bonus_to_offer])
         else:
-            vector.append(0)
+            vector.append(-1)
     return vector
 
 def vectorize_for_neighbours_predict(row, params: ServiceParams):
@@ -61,16 +60,18 @@ def vectorize_for_neighbours_predict(row, params: ServiceParams):
     vector.append(rating)
     return vector
 
-def predict_bonus(client_id, store_id, model, client_database, merchant_database, service_params: ServiceParams) -> str:
+def predict_bonus(client_id, store_id, rating: float, model, client_database, merchant_database, service_params: ServiceParams) -> str:
     if client_id not in client_database.index:
         return ''
     client_record = client_database.iloc[client_id]
+    client_record.rating = rating
     client_vector = vectorize_for_bonus_predict(client_record, store_id, merchant_database, service_params, train=False)
     client_vector = np.array(client_vector).astype(np.float32)
     prediction_pdf = model.predict_proba(client_vector.reshape(1,-1))[0]
-    predict_index = prediction_pdf.argmax()
-    if predict_index >= 1:
-        return service_params.bonus_type[predict_index-1], prediction_pdf[predict_index]
+    predict_arg = prediction_pdf.argmax()
+    predict_index = model.classes_[predict_arg]
+    if predict_index >= 0:
+        return service_params.bonus_type[predict_index], prediction_pdf[predict_arg]
     else:
         return '', 0.0
     
@@ -85,4 +86,5 @@ def predict_neighbours(client_id, model, client_database, service_params: Servic
     model.fit(X)
     distances, indices = model.kneighbors(client_vector.reshape(1,-1))
     distances = distances[0]
-    return indices[0][1:4], distances
+    max_distance = 240_000
+    return indices[0][1:4], 1-distances[1:4]/max_distance
